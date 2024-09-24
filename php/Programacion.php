ae1f78b97870bb383ac4d9d552125bbac0957160
salvador0001 <2330268@upv.edu.mx>
2024-09-23 21:25:33 -0600

    Plantillas de listas

---
+++ php/Programacion.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Programación</title>
+    
+    <link rel="stylesheet" href="../css/inicio.css">
+
+     <!-- App css -->
+     <link href="../layouts/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+     <link href="../layouts/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+     <link href="../layouts/assets/css/theme.min.css" rel="stylesheet" type="text/css" />
+
+</head>
+<body>
+    <div class="header">
+        <img src="../icons/menu.png" alt="">
+        <p>Programación</p>
+        <img src="../icons/campana.png" alt="">
+    </div>
+    <div class="content">
+        <div class="menu">
+            <p><a href="programacion.php">Programación</a></p>
+            <p><a href="mecatronica.php">Mecatronica</a></p>
+            <p><a href="Plasticos.php">Transformación de Plásticos</a></p>
+            <p><a href="propedeutico.php">Curso propedeutico</a></p>
+            <p><a href="Ayuda.php">Ayuda</a></p>
+        </div>
+
+        </div>
+        <main>
+            
+        </main>
+
+
+    <!-- jQuery  -->
+    <script src="../layouts/assets/js/jquery.min.js"></script>
+    <script src="../layouts/assets/js/bootstrap.bundle.min.js"></script>
+    <script src="../layouts/assets/js/theme.js"></script>
+</body>
+</html>
