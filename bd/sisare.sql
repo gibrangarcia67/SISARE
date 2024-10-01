dc4bce98210a19f53735f1117c71153d63e53990
CesarHdz22 <2330085@upv.edu.mx>
2024-10-01 09:48:42 -0600

    BD actualizada

--- bd/sisare.sql
+++ bd/sisare.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 26-09-2024 a las 08:32:16
+-- Tiempo de generación: 27-09-2024 a las 11:19:59
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -48,7 +48,7 @@ CREATE TABLE `alumnos` (
 --
 
 INSERT INTO `alumnos` (`grupo`, `especialidad`, `matricula`, `nombre`, `genero`, `nombre_p`, `correo_p`, `nombre_m`, `correo_m`, `cantidad_r`, `cantidad_s`, `cantidad_c`, `cantidad_cc`) VALUES
-('1E', 'COMPONENTE BASICO Y PROPEDEUTICO', 24328052710162, 'BERNAL CHAIRES GENESIS MIRANDA', 'M', '', '', '', '', 0, 0, 0, 0);
+('1E', 'COMPONENTE BASICO Y PROPEDEUTICO', 24328052710162, 'BERNAL CHAIRES GENESIS MIRANDA', 'M', '', '', 'CESAR ALEJANDRO HERNANDEZ MEDINA', '', 0, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -75,10 +75,15 @@ CREATE TABLE `cartacompromiso` (
 
 CREATE TABLE `citatorios` (
   `id` int(11) NOT NULL,
-  `dia` int(11) NOT NULL,
-  `mes` int(11) NOT NULL,
-  `ano` int(11) NOT NULL,
-  `hora` int(11) NOT NULL,
+  `dia_e` int(11) NOT NULL,
+  `mes_e` int(11) NOT NULL,
+  `ano_e` int(11) NOT NULL,
+  `dia_c` int(11) NOT NULL,
+  `mes_c` int(11) NOT NULL,
+  `ano_c` int(11) NOT NULL,
+  `hora_c` int(11) NOT NULL,
+  `min_c` int(11) NOT NULL,
+  `semestre` int(11) NOT NULL,
   `matricula` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -161,25 +166,25 @@ ALTER TABLE `suspensiones`
 -- AUTO_INCREMENT de la tabla `cartacompromiso`
 --
 ALTER TABLE `cartacompromiso`
-  MODIFY `Id_carta` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id_carta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `citatorios`
 --
 ALTER TABLE `citatorios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `reportes`
 --
 ALTER TABLE `reportes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `suspensiones`
 --
 ALTER TABLE `suspensiones`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
